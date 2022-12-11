984fa738635b5f2d22c1aee0dbae2cec3916f855
Vivek Shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2022-12-11 21:45:31 +0530

    test: Add test cases on ints and strings for heapsort

--- tests/test_heapsort.py
+++ tests/test_heapsort.py
@@ -4,8 +4,17 @@ from binheap import heapsort
 
 
 class TestHeapSort:
-    def test_heapsort(self):
+    def test_heapsort_on_list_of_ints(self):
         items = [randrange(100) for _ in range(100)]
+
+        # ascending sort
+        assert heapsort(items) == sorted(items)
+
+        # descending sort
+        assert heapsort(items, reverse=True) == sorted(items, reverse=True)
+
+    def test_heapsort_on_strings(self):
+        items = ''.join([chr(randrange(97, 123)) for _ in range(100)])
 
         # ascending sort
         assert heapsort(items) == sorted(items)
